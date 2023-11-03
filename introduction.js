af5ae762cc0c24937b34e93a8cb0098e730bc710
Sriaakash5 <73570910+Sriaakash5@users.noreply.github.com>
2023-11-03 13:55:19 +0530

    first-commit

---
+++ introduction.js
@@ -0,0 +1,1 @@
+console.log("Hey! 30 days into javascript");
---
+++ variable.js
@@ -0,0 +1,9 @@
+let firstName="Sri"
+let LastName="Aakash"
+let age=21
+let city="Chennai"
+let country="India"
+let universe;
+let GirlfriendName=null;
+
+console.log(firstName,LastName,age,city,country,universe,GirlfriendName);
